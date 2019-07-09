35f3c657474938e748d398970780017dc26d112d
archish <archishthakkar@gmail.com>
2019-07-09 16:00:11 +0530

    Updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,25 @@
+const config = require("./src/config");
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: config.siteTitle,
+    siteUrl: config.siteUrl,
+    author: config.name,
+    description: config.siteDescription,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `content`,
+        path: `${__dirname}/src/content`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -18,17 +27,50 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: config.name,
+        short_name: config.name,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: config.darkNavyColor,
+        theme_color: config.navyColor,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/src/content/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1100,
+              quality: 90,
+              linkImagesToOriginal: true,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: config.googleAnalyticsID,
+      },
+    },
   ],
 }
---
+++ src/config/index.js
@@ -0,0 +1,79 @@
+module.exports = {
+    siteTitle: 'Archish Thakkar | Software Engineer',
+    siteDescription:
+      'Archish Thakkar is a software engineer based in Mumbai, India who specializes in developing (and occasionally designing) exceptional, high-quality softwares, websites and applications.',
+    siteKeywords:
+      'Archish Thakkar, Archish, Thakkar, archish27, software engineer, front-end engineer, web developer, javascript, jpmorgan',
+    siteUrl: '',
+    siteLanguage: 'en_US',
+  
+    googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  
+    name: 'Archish Thakkar',
+    location: 'Mumbai, India',
+    email: 'archishthakkar@gmail.com',
+    github: 'https://github.com/archish27/',
+    socialMedia: [
+      {
+        name: 'Github',
+        url: 'https://github.com/bchiang7/',
+      },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/archish-thakkar/',
+      },
+      {
+        name: 'Stack Overflow',
+        url: 'https://twitter.com/archishthakkar',
+      },
+      {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/archis_thakkar/',
+      },
+
+    ],
+  
+    navLinks: [
+      {
+        name: 'About',
+        url: '#about',
+      },
+      {
+        name: 'Experience',
+        url: '#jobs',
+      },
+      {
+        name: 'Work',
+        url: '#projects',
+      },
+      {
+        name: 'Contact',
+        url: '#contact',
+      },
+    ],
+  
+    googleAnalyticsID: 'UA-78946797-1',
+  
+    navHeight: 100,
+  
+    greenColor: '#64ffda',
+    navyColor: '#0a192f',
+    darkNavyColor: '#020c1b',
+  
+    srConfig: (delay = 200) => ({
+      origin: 'bottom',
+      distance: '20px',
+      duration: 500,
+      delay,
+      rotate: { x: 0, y: 0, z: 0 },
+      opacity: 0,
+      scale: 1,
+      easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+      mobile: true,
+      reset: false,
+      useDelay: 'always',
+      viewFactor: 0.25,
+      viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+    }),
+  };
+  
