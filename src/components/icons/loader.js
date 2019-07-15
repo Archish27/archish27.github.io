4b42eb5b19be84000f4cc6472bf395c660aeb41c
archish <archishthakkar@gmail.com>
2019-07-15 08:10:30 +0530

    Bug fix preloader colors

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { theme } from '@styles';
+const { colors } = theme;
 
 const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Loader Logo</title>
     <g>
       <g id="A">
-        <path d="M24,62 L42,26 L60,62 L42,52 Z" fill="#0080F3" />
+        <path d="M24,62 L42,26 L60,62 L42,52 Z" fill={colors.blue} />
       </g>
       <path
-        stroke="#fff"
+        stroke={colors.white}
         d="M 32, 52
-                  L 42, 44.5
-                  L 52, 52
-                  L 42, 32 Z"
+                    L 42, 44.5
+                    L 52, 52
+                    L 42, 32 Z"
       />
-      <circle cx="42" cy="44" r="36" stroke="#0080F3" strokeWidth="2" />
+
+      <circle cx="42" cy="44" r="36" stroke={colors.blue} strokeWidth="2" />
     </g>
   </svg>
 );
